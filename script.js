f20edf657c613f24bb2d0fe071491abdfc6f0609
Josiel Lopez <jdlopez114@gmail.com>
2022-02-20 23:01:28 -0500

    Working button to randomize colors

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
+
+const button = document.getElementById('pressButton');
+let body = document.querySelector('body');
+
+function randomize() {
+    body.style.color = randomColors();
+  }
+  
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
